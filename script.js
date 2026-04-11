04d44845f62c88296b7b4a3769f826d995c60988
rosetwala45-oss <rosetwala45@gmail.com>
2026-04-11 16:41:32 +0200

    Add weather refresh handler and initial load
    Introduce refreshWeather(response) to update city and temperature from the API response and wire it into searchCity via axios.get(...).then(refreshWeather) (removed the console.log and the previous manual city DOM overwrite). Trigger an initial searchCity("Paris") on page load to populate the UI. Also adjust .weather-temperature font-weight from bold to 600 for styling consistency.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // THE ONE FUNCTION THAT WILL UPDATE EVERYTHING
 
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   //make api call and update the UI
   let apiKey = "0dc6tacc431o670b15e9a5cb213feec1";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
@@ -12,9 +21,8 @@ function handleSearchSubmit(event) {
 
   //   Getting input element
   let searchInput = document.querySelector("#search-form-input");
-  // Replacng h1 with the value from input
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  // Replacng h1 with city from API
+
   // Geiing value for API call
   searchCity(searchInput.value);
 }
@@ -23,3 +31,6 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 // What must happen when form is submitted:
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+// Call city on page load:
+searchCity("Paris");
