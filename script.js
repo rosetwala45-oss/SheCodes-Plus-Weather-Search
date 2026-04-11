3046a294b2f13d8294ff4a39060d630d92c7af83
rosetwala45-oss <rosetwala45@gmail.com>
2026-04-11 16:21:06 +0200

    Add searchCity and fix search input typo
    Introduce searchCity(city) which builds the SheCodes weather API URL (and logs it) to prepare for fetching/updating the UI. Fix a typo by renaming seachInput to searchInput, use it to set the #city text, and call searchCity on form submit.

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// DECLARING FUNCTIONS:
+// THE ONE FUNCTION THAT WILL UPDATE EVERYTHING
+
+function searchCity(city) {
+  //make api call and update the UI
+  let apiKey = "0dc6tacc431o670b15e9a5cb213feec1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
 
   //   Getting input element
-  let seachInput = document.querySelector("#search-form-input");
-
+  let searchInput = document.querySelector("#search-form-input");
   // Replacng h1 with the value from input
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = seachInput.value;
+  cityElement.innerHTML = searchInput.value;
+  // Geiing value for API call
+  searchCity(searchInput.value);
 }
 
 // Setting a variable for the form:
 let searchFormElement = document.querySelector("#search-form");
-
 // What must happen when form is submitted:
 searchFormElement.addEventListener("submit", handleSearchSubmit);
